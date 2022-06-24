0e5beecb993e0a6d956e5c066946871c7946d749
ToobaZainab <toobazia59@gmail.com>
2022-06-24 17:06:50 +0500

    Hello from Tooba

---
+++ Array.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class week
+{
+	private:
+	int n;
+	public:
+	void input(int n)
+	cout<<"Friday";
+	cin>>1;
+	cout<<"Saturday";
+	cin>>2;
+	cout<<"Sunday";
+	cin>>3;
+	cout<<"Monday";
+	cin<<4;
+	cout<<"Tuesday";
+	cin>>5;
+	cout<<"Wednesday";
+	cin>>6;
+	cout<<"Thursday";
+	cin>>7;
+}
+   void output(){
+   	cout<<"Enter number of weekday:"<<n<<endl;
+   	
+   }
+   void public::input(int n)
+};
+    int main()
+    {
+    week x;
+    x.input();
+    x.output();
+    return 0;
+	}
